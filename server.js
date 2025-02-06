056e4557edf278217fcb44104fea74f8b3e36a10
Griffins2005 <Griffins2005>
2025-02-06 11:04:24 -0500

    updated cors permissions

--- server.js
+++ server.js
@@ -19,10 +19,18 @@ const allowedOrigins = ['https://shopsphere-ecommerce.vercel.app'];
 app.use(
   cors({
     origin: allowedOrigins,
-    methods: 'GET,UPDATE,POST,PUT,DELETE',
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins[0]);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(passport.initialize());
 app.use(express.json());
