7f82575a42a2bada1cc5a07088f849d278e86699
Griffins2005 <Griffins2005>
2025-02-06 11:21:32 -0500

    cors permissions

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require("dotenv");
-dotenv.config();
-const passport = require("./passport.js");
+const dotenv = require('dotenv');
+const passport = require('./passport.js');
+const crypto = require('crypto');
 
+dotenv.config();
+
+// Import routes
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
-const cartRoutes = require("./routes/cart");
-const wishlistRoutes = require("./routes/wishlist");
-const storeRoutes = require("./routes/store");
-const checkoutRoutes = require("./routes/checkout");
+const cartRoutes = require('./routes/cart');
+const wishlistRoutes = require('./routes/wishlist');
+const storeRoutes = require('./routes/store');
+const checkoutRoutes = require('./routes/checkout');
 
 const app = express();
 
@@ -19,33 +22,36 @@ const allowedOrigins = ['https://shopsphere-ecommerce.vercel.app'];
 app.use(
   cors({
     origin: allowedOrigins,
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins[0]);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Content-Security-Policy","script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com");
+  const nonce = Buffer.from(crypto.randomBytes(16)).toString('base64');
+  res.header('Access-Control-Allow-Origin', allowedOrigins[0]);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader(
+    'Content-Security-Policy',
+    `script-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://accounts.google.com;`
+  );
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
   next();
 });
-app.use((req, res, next) => {
-  
-});
-
 
 app.use(passport.initialize());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/wishlist", wishlistRoutes);
-app.use("/api/store", storeRoutes);
-app.use("/api/checkout", checkoutRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/wishlist', wishlistRoutes);
+app.use('/api/store', storeRoutes);
+app.use('/api/checkout', checkoutRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {})
