db7eb69d34a96e283813f6e2276108e0816014fb
Griffins2005 <Griffins2005>
2025-02-06 22:17:51 -0500

    update cors

--- server.js
+++ server.js
@@ -15,30 +15,13 @@ const checkoutRoutes = require('./routes/checkout');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000', 'https://shopsphere-ecommerce.vercel.app'];
+app.use(cors({
+  origin: ['https://shopsphere-ecommerce.vercel.app'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
-
-app.use(
-  cors({
-    origin: ['https://shopsphere-ecommerce.vercel.app', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 app.use(passport.initialize());
